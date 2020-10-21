54f2c4d53d8d5c9cc2855afc20f3a69591384c13
Kuma121 <darth.pascal1@web.de>
2020-10-21 16:20:39 +0200

    Version 0.1

---
+++ MetrikSoftware/src/MetrikSoftware.java
@@ -0,0 +1,15 @@
+public class MetrikSoftware {
+	
+	public static void main(String[] args) {
+		
+		int windowSizeX = 500;
+		int windowSizeY = 500;
+		int color = 0;
+
+//		MainWindow mw = new MainWindow(windowSizeX, windowSizeY);
+//		mw.openWindow();
+		MainWindow mw = new MainWindow(windowSizeX, windowSizeY, color);
+		mw.openWindow();
+	}
+
+}
